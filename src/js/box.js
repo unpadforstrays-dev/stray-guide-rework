bd6d0150470e4a6563d243096daf1b4eb5d01c0a
Michelle <firasav99@gmail.com>
2025-11-29 20:31:41 +0700

    fix svg pake cdn

--- src/js/box.js
+++ src/js/box.js
@@ -20,13 +20,13 @@ class FancyCard extends HTMLElement {
       // Kamu lupa nambahin OR di initialization untuk default case (tips)
       switch (tipe) {
         case 'warn':
-          icon = "https://raw.githubusercontent.com/unpadforstrays-dev/stray-guide-rework/main/public/tips.svg";
+          icon = "https://raw.githack.com/unpadforstrays-dev/stray-guide-rework/main/public/warn.svg";
           break;
         case 'info':
-          icon = "https://raw.githubusercontent.com/unpadforstrays-dev/stray-guide-rework/raw/refs/heads/main/public/info.svg";
+          icon = "https://raw.githack.com/unpadforstrays-dev/stray-guide-rework/main/public/info.svg";
           break;
         default:
-          icon = "https://raw.githubusercontent.com/unpadforstrays-dev/stray-guide-rework/raw/refs/heads/main/public/tips.svg";
+          icon = "https://raw.githack.com/unpadforstrays-dev/stray-guide-rework/main/public/tips.svg";
       }
       
       this.innerHTML = `
@@ -40,5 +40,4 @@ class FancyCard extends HTMLElement {
     }
   }
   
-
   customElements.define('fancy-card', FancyCard);  
